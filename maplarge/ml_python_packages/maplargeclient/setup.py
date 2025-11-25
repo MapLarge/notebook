e47c0d4d9c7c6ac024706f3cb36e58085d5dfd0e
Ben Mayer <ben.mayer@maplarge.com>
2025-11-25 08:06:14 -0600

    Bumb version to 4.78.5

--- maplarge/ml_python_packages/maplargeclient/setup.py
+++ maplarge/ml_python_packages/maplargeclient/setup.py
@@ -1,3 +1,7 @@
 from setuptools import find_packages, setup
 
-setup(name="maplargeclient", version="4.78.4", packages=find_packages())
+setup(
+	name="maplargeclient",
+	version="4.78.5",
+	packages=find_packages()
+)
